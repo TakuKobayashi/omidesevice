b525d37637d252bee082530bce9b3127b6d282c5
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-11-19 13:18:20 +0900

    first commit

---
+++ android/app/src/main/java/kobayashi/taku/com/omoideservice/MainActivity.java
@@ -0,0 +1,13 @@
+package kobayashi.taku.com.omoideservice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
